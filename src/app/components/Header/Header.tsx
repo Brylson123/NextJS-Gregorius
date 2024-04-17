79f3d0c88b90b950326eb50d70ccb4f19d737f08
Bryła <csowscy@gmail.com>
2024-04-17 17:56:57 +0200

    feat: add pricing section

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -20,8 +20,8 @@ const callouts = [
 ]
 export default function Header() {
     return (
-        <header className="w-full h-full bg-blue-300">
-            <div className="bg-blue-100">
+        <header className="w-full h-full">
+            <div className="bg-blue-100 h-full">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
                         <h2 className="text-2xl font-bold text-gray-900">Kim jesteśmy?</h2>
