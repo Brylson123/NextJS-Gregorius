4a09fbdbea86edcdddc103ac70441c2e33905816
Bryła <csowscy@gmail.com>
2024-04-18 09:12:21 +0200

    feat: add id for nav

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -20,7 +20,7 @@ const callouts = [
 ]
 export default function Header() {
     return (
-        <header className="w-full h-full">
+        <header className="w-full h-full" id="about">
             <div className="bg-blue-100 h-full">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
